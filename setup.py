15ea62b0895e89cb47a5bb1a41773a957e1d8597
kiorky <kiorky@cryptelium.net>
2011-01-25 17:52:50 +0100

    proper packaging

--- setup.py
+++ setup.py
@@ -1,35 +1,20 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-import sys
-import unittest
-
-import croniter
-from croniter import __version__, __license__, __author__
-
-if __name__ == '__main__':
-  from croniter import croniter_test
-  # run module test
-  loader = unittest.TestLoader()
-  result = unittest.TestResult()
-  suite  = loader.loadTestsFromModule(croniter_test)
-  suite.run(result)
-  if not result.wasSuccessful():
-    print "unit tests have failed!"
-    print "aborted to make a source distribution"
-    sys.exit(1)
-
-  # build distribution package
-  setup(
+__version__, __doc__, __license__, __author__ = None, None, None, None
+# get __version__, __doc__, __license__, __author__
+execfile("croniter/_release.py")
+setup(
     packages         = ('croniter',),
     name             = 'croniter',
     version          = __version__,
     py_modules       = ['croniter', 'croniter_test'],
     description      = 'croniter provides iteration for datetime object with cron like format',
-    long_description = croniter.__doc__,
+    long_description = __doc__,
     author           = __author__,
     author_email     = 'taichino@gmail.com',
     url              = 'http://github.com/taichino/croniter',
     keywords         = 'datetime, iterator, cron',
+    install_requires = ["python-dateutil", "setuptools",],
     license          = __license__,
     classifiers      = ["Development Status :: 3 - Alpha",
                         "Intended Audience :: Developers",
@@ -37,4 +22,4 @@ if __name__ == '__main__':
                         "Operating System :: POSIX",
                         "Programming Language :: Python",
                         "Topic :: Software Development :: Libraries :: Python Modules"]
-    )
+)
