9f3ce1af03717f0942cb369479829d95237e86ba
Matsumoto Taichi <taichino@gmail.com>
2012-09-25 23:11:04 -0400

    update version to 0.3.1

--- croniter/_release.py
+++ croniter/_release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__  = "Matsumoto Taichi"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __license__ = "MIT License"
 __doc__ = """
     Simple example of usage is followings
