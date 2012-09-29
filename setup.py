1ba6dea96a1d27fa3f28f67b1cfb644f95976f93
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-09-29 18:21:05 +0200

    Preparing release 0.3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name             = 'croniter',
-    version          = "0.3.3dev",
+    version='0.3.3',
     py_modules       = ['croniter', ],
     description      = 'croniter provides iteration for datetime object with cron like format',
     long_description = long_description,
