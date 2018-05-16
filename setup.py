2c668a8581136a18262bf1a55f21f1d51488c4ca
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-05-16 16:22:11 +0200

    Preparing release 0.3.21

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = "\n\n".join(
 
 setup(
     name='croniter',
-    version='0.3.21.dev0',
+    version='0.3.21',
     py_modules=['croniter', ],
     description=(
         'croniter provides iteration for datetime '
