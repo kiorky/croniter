0c3eb2fdcad2d1b21b4d5a0ecf338bdd49f93d1f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-01-02 16:22:23 +0100

    Preparing release 0.3.31

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = "\n\n".join(
 
 setup(
     name='croniter',
-    version='0.3.31.dev0',
+    version='0.3.31',
     py_modules=['croniter', ],
     description=(
         'croniter provides iteration for datetime '
