dacbee6483068c81e53a2af5c04a1563dd77883a
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-28 15:52:44 +0100

    Add Python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     packages=find_packages('src', exclude=['tests*', '*.tests*']),
     package_dir={'': 'src'},
