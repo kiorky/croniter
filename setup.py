46c4578c437bf61e2e7e912b972c9b224bf379b8
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2021-07-15 21:12:39 +0100

    Fix #155: raise CroniterBadCronError when error syntax

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = "\n\n".join(
 
 setup(
     name='croniter',
-    version='1.0.3.dev0',
+    version='1.0.4.dev0',
     py_modules=['croniter', ],
     description=(
         'croniter provides iteration for datetime '
