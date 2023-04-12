ce6bde003064ba0fdc94ca7589d34834c59e0ce0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-04-12 18:04:06 +0200

    Back to development: 1.3.14

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = "\n\n".join(
 
 setup(
     name='croniter',
-    version='1.3.13',
+    version='1.3.14.dev0',
     py_modules=['croniter', ],
     description=(
         'croniter provides iteration for datetime '
